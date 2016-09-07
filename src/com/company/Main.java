de4aaccab21af3c36c6179692f11aefa48949d2f
Thomas H O'Hara <to007@pvhs-lab112-d18.pvsd.org>
2016-09-07 14:20:46 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World!");
+        System.out.print("What is going on?");
+        //Two ways that we need to print
+        System.out.print(1);
+        System.out.print(1.0);
+    }
+}
